a8c25d95c50db84bf3300d1694d6e6341d7db44c
alex <alexnakagama4@gmail.com>
2026-04-01 19:06:36 -0300

    added database models and database connection

---
+++ app/models/User.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, index=True)
+    hashed_pw = Column(String)
+    
+    tasks = relationship(
+        "Task", 
+        back_populates="owner",
+        # if a user is deleted all his tasks are deleted as well
+        cascade="all, delete-orphan"
+        )
