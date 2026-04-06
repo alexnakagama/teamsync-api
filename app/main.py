adb5645e387d7ea880fb363d9ea29c8ab8b30e91
alex <alexnakagama4@gmail.com>
2026-04-06 01:59:01 -0300

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
+
 from app.core.limiter import limiter
 
 from app.api.routers.users_router import users_router
 from app.api.routers.manager_router import manager_router
+
 from app.db.database import Base, engine
+
 from app.models.user import User
 from app.models.task import Task
 
@@ -14,6 +19,14 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(manager_router)
